0a9792c8822bc6284c3e798e8e68f8b1cb2fd0f0
Daniel <fostiakd@gmail.com>
2023-08-28 23:25:23 +0100

    css styling

--- src/App.jsx
+++ src/App.jsx
@@ -31,89 +31,107 @@ function App() {
   }
 
   return (
-    <main className="prose container mx-auto flex flex-col items-center">
-      <h1 className="mt-4">Sentiment Analysis</h1>
-      <h4 className="text-center">
-        Sentiment analysis, a branch of natural language processing, involves
-        using computational methods to classify and quantify the emotional tone
-        expressed in text data.
-      </h4>
-      <h2>Upload text</h2>
-      <textarea
-        type="textarea"
-        onChange={(e) => setText(e.target.value)}
-        value={text}
-        className="textarea textarea-primary m-2 textbox"
-        ref={textAreaRef}
-        rows={1}
-      ></textarea>
-      <button className="btn btn-primary text-center" onClick={handleSubmit}>
-        Submit
-      </button>
-      {loading && <span class="m-5 loading loading-spinner loading-lg"></span>}
-      {analysis && (
-        <div className="container flex flex-col items-center">
-          <div className="stats shadow">
-            <div className="stat">
-              <div className="stat-title text-center">Score</div>
-              <div className="stat-value text-center">{`${
-                score > 0 ? "+" : ""
-              }${score}`}</div>
-              <div className="stat-desc text-center">
-                Ranked between -5 and 5
+    <div className="App">
+      <main className="prose container mx-auto flex flex-col items-center">
+        <h1 className="mt-4">Sentiment Analysis</h1>
+        <h4 className="text-center">
+          Sentiment analysis, a branch of natural language processing, involves
+          using computational methods to classify and quantify the emotional
+          tone expressed in text data.
+        </h4>
+        <h2>Upload text</h2>
+        <textarea
+          type="textarea"
+          onChange={(e) => setText(e.target.value)}
+          value={text}
+          className="textarea textarea-primary m-2 textbox"
+          ref={textAreaRef}
+          rows={1}
+        ></textarea>
+        <button className="btn btn-primary text-center" onClick={handleSubmit}>
+          Submit
+        </button>
+        {loading && (
+          <span class="m-5 loading loading-spinner loading-lg"></span>
+        )}
+        {analysis && (
+          <div className="container flex flex-col items-center">
+            <div className="stats shadow">
+              <div className="stat">
+                <div className="stat-title text-center">Score</div>
+                <div className="stat-value text-center">{`${
+                  score > 0 ? "+" : ""
+                }${score}`}</div>
+                <div className="stat-desc text-center">
+                  Ranked between -5 and 5
+                </div>
               </div>
             </div>
+            <progress
+              className={`progress progress${
+                score ? (score > 0 ? "-success" : "-error") : ""
+              } w-56`}
+              value={+score + 5}
+              max="10"
+            ></progress>
+            <h2>Why?</h2>
+            <p>
+              Our program gave this{" "}
+              {score < -1
+                ? "poor score as there was a high frequency of negative words"
+                : score > 1
+                ? "high score as there was high frequency of positive words"
+                : "mediocre score as there was a lot of varied/neutral words"}
+              {"."}
+            </p>
+            <div className="overflow-x-auto">
+              <table className="table">
+                {/* head */}
+                <thead>
+                  <tr>
+                    <th></th>
+                    <th>Word</th>
+                    <th>Sentiment</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {words.map((word, i) => {
+                    const [w, s] = Object.entries(word)[0];
+                    return (
+                      <tr key={i}>
+                        <th>{i + 1}</th>
+                        <td>{w}</td>
+                        <td>{s}</td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
+            </div>
+            <p>
+              The algorithm works by assigning a sentiment score to each word.
+              Most words are neutral so don't get a score. Each score is between
+              -5 and 5. After, we sum up the scores, we then divide by the total
+              number of words to get the overall score.
+            </p>
           </div>
-          <progress
-            className={`progress progress${
-              score ? (score > 0 ? "-success" : "-error") : ""
-            } w-56`}
-            value={+score + 5}
-            max="10"
-          ></progress>
-          <h2>Why?</h2>
+        )}
+      </main>
+      <footer
+        className={`footer footer-center p-4 bg-base-300 text-base-content ${
+          analysis ? "" : "fixed"
+        } bottom-0`}
+      >
+        <div>
           <p>
-            Our program gave this{" "}
-            {score < -1
-              ? "poor score as there was a high frequency of negative words"
-              : score > 1
-              ? "high score as there was high frequency of positive words"
-              : "mediocre score as there was a lot of varied/neutral words"}{" "}
+            Developed by Daniel Fostiak. Code @
+            <a className="link link-primary ">
+              https://github.com/danielfostiak/sentimentanalysis
+            </a>
           </p>
-          <div className="overflow-x-auto">
-            <table className="table">
-              {/* head */}
-              <thead>
-                <tr>
-                  <th></th>
-                  <th>Word</th>
-                  <th>Sentiment</th>
-                </tr>
-              </thead>
-              <tbody>
-                {words.map((word, i) => {
-                  const [w, s] = Object.entries(word)[0];
-                  return (
-                    <tr key={i}>
-                      <th>{i + 1}</th>
-                      <td>{w}</td>
-                      <td>{s}</td>
-                    </tr>
-                  );
-                })}
-              </tbody>
-            </table>
-          </div>
-          <p>
-            The algorithm works by assigning a sentiment score to each word.
-            Most words are neutral so don't get a score. Each score is between
-            -5 and 5. After, we sum up the scores, we then divide by the total
-            number of words to get the overall score.
-          </p>
-          <p>If you find any issues, visit </p>
         </div>
-      )}
-    </main>
+      </footer>
+    </div>
   );
 }
 
